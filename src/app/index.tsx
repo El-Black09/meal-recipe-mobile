e981a7ef9de343e024914eed15c08144e08c4cd3
Arnaud Igor N'takpe <arnaud.ntakpe@mpayment.africa>
2026-03-24 15:12:59 +0000

    feat: add styles files and constants MRM-1

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Image style={styles.image} source={require("@/assets/images/logo-glow.png")}/>
     </View>
   );
 }
@@ -13,5 +15,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    backgroundColor: "#0553",
   },
 });
